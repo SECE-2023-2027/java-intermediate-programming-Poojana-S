fc5f7eb5f7d9a24c7b67ab7f9f1951f976fd1d1b
08021976-22022006 <poojanas.dev@gmail.com>
2026-07-01 11:57:26 +0530

    Problem  with input1

--- Problem 1/Main.java
+++ Problem 1/Main.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        Product p = new Product("P101", "Laptop", 1000.0, 15);
-        p.applyDiscount(10);
+        Scanner sc = new Scanner(System.in);
+        String id=sc.next();
+        String name=sc.next();
+        double price=sc.nextDouble();
+        int stock=sc.nextInt();
+        Product p = new Product(id, name, price, stock);
+        System.out.println("Input discount");
+        double percent=sc.nextDouble();
+        p.applyDiscount(percent);
         System.out.println(p.getPrice());
         System.out.println(p.getStockQuantity());
     }
